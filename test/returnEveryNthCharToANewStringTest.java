0ddef53f31659081d4e338ae6ccd65b75917da05
April Howard <ahoward@zipcodes-MBP-5.fios-router.home>
2020-05-28 02:25:13 -0400

    Junit test for everyNth method

--- test/returnEveryNthCharToANewStringTest.java
+++ test/returnEveryNthCharToANewStringTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,6 +8,10 @@ public class returnEveryNthCharToANewStringTest {
     @Test
     public void everyNth() {
 
-
+        Assert.assertEquals("Cla", returnEveryNthCharToANewString.everyNth("Chelsea", 3));
+        Assert.assertEquals("Geoy", returnEveryNthCharToANewString.everyNth("Gregory", 2));
+        Assert.assertEquals("Ai", returnEveryNthCharToANewString.everyNth("April", 3));
+        Assert.assertEquals("Zba", returnEveryNthCharToANewString.everyNth("Zebra", 2));
+        Assert.assertEquals("Error.", returnEveryNthCharToANewString.everyNth("April", 6));
     }
 }
